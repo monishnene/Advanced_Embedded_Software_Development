5c4be1a3f456656d24aecb97a63b3b166af71573
monishnene <monish.nene@colorado.edu>
2019-01-24 19:19:38 -0700

    bash done

--- HW1/system_info.sh
+++ HW1/system_info.sh
@@ -1,1 +1,16 @@
-to be written
+rm -f systeminfo.txt
+echo username $(whoami)>>systeminfo.txt
+echo >>systeminfo.txt
+echo Operating System Type/Brand>>systeminfo.txt
+cat /etc/*-release>>systeminfo.txt
+echo >>systeminfo.txt
+echo Kernel Version>>systeminfo.txt
+uname -r>>systeminfo.txt
+echo >>systeminfo.txt
+cat /proc/version>>systeminfo.txt
+echo >>systeminfo.txt
+echo architecture $(arch)>>systeminfo.txt
+echo >>systeminfo.txt
+echo Information on File System Memory>>systeminfo.txt
+free>>systeminfo.txt
+
