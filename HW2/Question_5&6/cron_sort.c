1c7648e9235b2c3dea389912bcedde22aa887ed0
monishnene <monish.nene@colorado.edu>
2019-02-10 22:49:48 -0700

    saved table and .h

--- HW2/Question_5&6/cron_sort.c
+++ HW2/Question_5&6/cron_sort.c
@@ -11,27 +11,6 @@
 #define True 1
 #define False 0
 
-void error(int32_t error_check)
-{
-	if(error_check==1002)
-	{	
-		printf("Error: NULL pointer passed as buffer\n");
-	}
-	else if(error_check==1001)
-	{	
-		printf("Error: Negative size passed\n");
-	}
-	else if(error_check==1000)
-	{
-		printf("Error: Data size is less than 256");
-	}
-	else
-	{
-		perror("\nError");
-	}
-    	exit(0);
-}
-
 void cronlog(int32_t count,int32_t* data)
 {
 	uint8_t* str= (uint8_t*) malloc(STRING_SIZE);
@@ -68,7 +47,22 @@ void main()
 	error_check=syscall(398,data,DATA_SIZE,output);
 	if(error_check!=0)
 	{
-		error(error_check);
+		if(error_check == 1002)
+		{	
+			printf("Error: NULL pointer passed as buffer\n");
+		}
+		else if(error_check == 1001)
+		{	
+			printf("Error: Negative size passed\n");
+		}
+		else if(error_check == 1000)
+		{
+			printf("Error: Data size is less than 256");
+		}
+		else
+		{
+			perror("\nError");
+		}
 	}
 	else
 	{
@@ -78,7 +72,22 @@ void main()
 	error_check=syscall(398,NULL,DATA_SIZE,output);
 	if(error_check!=0)
 	{
-		error(error_check);
+		if(error_check == 1002)
+		{	
+			printf("Error: NULL pointer passed as buffer\n");
+		}
+		else if(error_check == 1001)
+		{	
+			printf("Error: Negative size passed\n");
+		}
+		else if(error_check == 1000)
+		{
+			printf("Error: Data size is less than 256");
+		}
+		else
+		{
+			perror("\nError");
+		}
 	}
 	else
 	{
@@ -88,7 +97,22 @@ void main()
 	error_check=syscall(398,data,DATA_SIZE,NULL);
 	if(error_check!=0)
 	{
-		error(error_check);
+		if(error_check == 1002)
+		{	
+			printf("Error: NULL pointer passed as buffer\n");
+		}
+		else if(error_check == 1001)
+		{	
+			printf("Error: Negative size passed\n");
+		}
+		else if(error_check == 1000)
+		{
+			printf("Error: Data size is less than 256");
+		}
+		else
+		{
+			perror("\nError");
+		}
 	}
 	else
 	{
@@ -98,7 +122,22 @@ void main()
 	error_check=syscall(398,data,-33,output);
 	if(error_check!=0)
 	{
-		error(error_check);
+		if(error_check == 1002)
+		{	
+			printf("Error: NULL pointer passed as buffer\n");
+		}
+		else if(error_check == 1001)
+		{	
+			printf("Error: Negative size passed\n");
+		}
+		else if(error_check == 1000)
+		{
+			printf("Error: Data size is less than 256");
+		}
+		else
+		{
+			perror("\nError");
+		}
 	}
 	else
 	{
@@ -108,7 +147,22 @@ void main()
 	error_check=syscall(398,data,33,output);
 	if(error_check!=0)
 	{
-		error(error_check);
+		if(error_check == 1002)
+		{	
+			printf("Error: NULL pointer passed as buffer\n");
+		}
+		else if(error_check == 1001)
+		{	
+			printf("Error: Negative size passed\n");
+		}
+		else if(error_check == 1000)
+		{
+			printf("Error: Data size is less than 256");
+		}
+		else
+		{
+			perror("\nError");
+		}
 	}
 	else
 	{
--- HW2/Question_5&6/syscalls.h
+++ HW2/Question_5&6/syscalls.h
@@ -940,6 +940,6 @@ asmlinkage long sys_pkey_alloc(unsigned long flags, unsigned long init_val);
 asmlinkage long sys_pkey_free(int pkey);
 asmlinkage long sys_statx(int dfd, const char __user *path, unsigned flags,
 			  unsigned mask, struct statx __user *buffer);
-asmlinkage long sys_merge_sort(int *buffer, int size, int* sorted_buffer);
+asmlinkage long sys_merge_sort(int __user * buffer, int size, int __user * sorted_buffer);
 #endif
 
