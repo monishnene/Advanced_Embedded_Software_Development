58dd0d19ca7d0a930c9ef067bdf3bd09edbaa127
monishnene <monish.nene@colorado.edu>
2019-02-17 17:06:38 -0700

    set 1 done

--- HW3/Q4/data_structure.c
+++ HW3/Q4/data_structure.c
@@ -5,45 +5,121 @@
 #include <time.h>
 #include <string.h>
 #include <math.h>
+#include <search.h>
+#define ANIMAL_NAME_SIZE 12
 #define ARRAY_SIZE 50
 #define HASH_TABLE_SIZE (uint32_t)(ARRAY_SIZE*log(ARRAY_SIZE))
 
 uint8_t* hash_table;
 
-uint32_t hash(uint8_t* str)
+uint32_t bubble_sort_alphabetical(int32_t count,uint8_t** data)
 {
-	uint32_t key=1,i=0;
-	while(*(str+i)!=0)
+	uint8_t* temp;
+	int32_t i=0,j=0;
+	for(i=0;i<count;i++)
 	{
-		key=*(str+i)**(str+i++)*key;		
+		for(j=i;j<count;j++)
+		{
+			if(strcmp(*(data+i),*(data+j))>0)
+        		{
+            			temp=*(data+i);
+            			*(data+i)=*(data+j);
+            			*(data+j)=temp;
+        		}
+		}
 	}
-	key=key%HASH_TABLE_SIZE;
-	return key;
+	for(i=0;i<count-1;i++)
+	{
+		if(strcmp(*(data+i),*(data+i+1))==0)
+		{
+			for(j=i;j<count-1;j++)
+			{	
+				*(data+j)=*(data+j+1);
+        		}
+			count--;
+			i--;		
+		}
+	}
+	return count;
 }
+
 
 void main(int32_t argc, uint8_t **argv)
 {
+	if(argc<2)
+	{
+		printf("./data_structure.elf <animals>\n");
+		exit(0);
+	}
 	uint32_t i=0;
+	uint8_t* ptr;
+	int32_t unique_animals=argc-1;
+	uint8_t** unique_array=(uint8_t**)malloc(unique_animals*sizeof(uint8_t*));
+	ENTRY e,*ep;
 	srand(time(NULL));
 	uint8_t* animals[ARRAY_SIZE];
-	hash_table = calloc(HASH_TABLE_SIZE,1);
+	hash_table =(uint8_t*)calloc(unique_animals,1);
+	hcreate(HASH_TABLE_SIZE);
+	for(i=1;i<argc;i++)
+	{
+		*(unique_array+i-1)=(uint8_t*)malloc(ANIMAL_NAME_SIZE);
+		*(unique_array+i-1)=strcpy(*(unique_array+i-1),argv[i]);
+	}
+	unique_animals=bubble_sort_alphabetical(unique_animals,unique_array);
+	printf("\nUnique Animals in the Ecosystem are\n");
+	for(i=0;i<unique_animals;i++)
+	{
+		printf("%s\t",unique_array[i]);
+		e.key=unique_array[i];
+		ep=hsearch(e,ENTER);
+		if(ep==NULL)
+		{
+			fprintf(stderr,"Hash Entry Failed\n");
+			exit(EXIT_FAILURE);	
+		}
+		ep->data=(void*)(hash_table+i);
+	}
+	printf("\n\nAnimals in the Seed Array are\n");
 	for(i=0;i<ARRAY_SIZE;i++)
 	{
-		animals[i]=argv[(rand()%(argc-1))+1];	
+		animals[i]=(uint8_t*)malloc(ANIMAL_NAME_SIZE);
+		animals[i]=strcpy(animals[i],unique_array[rand()%unique_animals]);
+		printf("%s\t",animals[i]);
+		e.key=animals[i];
+		ep=hsearch(e,ENTER);
+		if(ep==NULL)
+		{
+			fprintf(stderr,"Hash Entry Failed\n");
+			exit(EXIT_FAILURE);	
+		}
+		ptr=(uint8_t*)(ep->data);
+		*(ptr)+=1;
 	}
-	for(i=0;i<ARRAY_SIZE;i++)
-	{
-		printf("%s ",animals[i]);
-		*(hash_table+hash(animals[i]))+=1;
-	}
-	printf("\nHash Values\n");
 	i=1;	
-	while(i<argc)
+	printf("\n");
+	printf("\nAnimals in Ecosystem\n");
+	for(i=0;i<unique_animals;i++)
 	{
-		printf("%s=%d key = %d\n",argv[i],*(hash_table+hash(argv[i])),hash(argv[i]));
-		i++;
+		e.key=unique_array[i];
+		ep=hsearch(e,ENTER);
+		if(ep==NULL)
+		{
+			fprintf(stderr,"Hash Entry Failed\n");
+			exit(EXIT_FAILURE);	
+		}
+		printf("%s=%d\n",unique_array[i],*(uint8_t*)(ep->data));
+		
+	}
+	hdestroy();
+	for(i=0;i<ARRAY_SIZE;i++)
+	{
+		free(animals[i]);
+	}
+	for(i=0;i<unique_animals;i++)
+	{
+		free(unique_array[i]);
 	}
 	free(hash_table);
-	printf("\n");
+	exit(EXIT_SUCCESS);
 }
 
